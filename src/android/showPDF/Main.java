b947806555b2b5287f8ea3d4e2e10c3e12fdc18a
Simar <441315491@qq.com>
2017-09-26 08:24:17 +0800

    'update'

---
+++ src/android/showPDF/Main.java
@@ -0,0 +1,55 @@
+package cordova.plugin.androidShowPDFPlugin;;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+import java.io.File;
+import simar.android.showpdf.R;
+
+public class Main extends Activity implements OnPageChangeListener, OnLoadCompleteListener {
+    private PDFView pdfView;
+    private String fileURL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_androidshowpdfplugin_main);
+        pdfView = (PDFView) findViewById(R.id.pdfView);
+        setPdfView();
+    }
+
+    private void setPdfView() {
+        Intent intent = getIntent();
+        fileURL = intent.getStringExtra("PDF_PATH");
+        if(fileURL!=null&&!"".equals(fileURL)){
+            File pdfFile = new File(fileURL);
+            pdfView.fromAsset(fileURL)
+                    .defaultPage(0)
+                    .onPageChange(this)
+                    .enableAnnotationRendering(true)
+                    .onLoad(this)
+                    .scrollHandle(new DefaultScrollHandle(this))
+                    .load();
+        }else{
+            Toast.makeText(this,getString(R.string.error_path),Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+
+    }
+
+    @Override
+    public void loadComplete(int nbPages) {
+
+    }
+}
---
+++ src/android/showPDF/hook/configure-android.js
@@ -0,0 +1,57 @@
+/**
+ * Created by p.messina on 14/10/2015.
+ */
+
+module.exports = function (ctx) {
+
+    if (ctx.opts.platforms.indexOf('android') < 0) {
+        return;
+    }
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        deferral = ctx.requireCordovaModule('q').defer();
+
+    function replace_string_in_file(filename, to_replace, replace_with) {
+        var data = fs.readFileSync(filename, 'utf8');
+        var result = data.replace(new RegExp(to_replace, "g"), replace_with);
+        fs.writeFileSync(filename, result, 'utf8');
+    }
+    
+    function getConfidId(configString){
+    	
+    	var firstCut = configString.split(" id=");
+		//console.log(firstCut);
+		var secondCut = firstCut[1].replace(/"/g,"");
+		//console.log(secondCut);
+		var id = secondCut.slice(0,secondCut.indexOf(" "));
+		//console.log(id);
+		return id;
+    }
+    
+    var ourconfigfile = path.join(ctx.opts.projectRoot, "config.xml");
+    var configXMLPath = "config.xml";
+    var data = fs.readFileSync(ourconfigfile, 'utf8');
+    
+    var replaceWith = getConfidId(data) + ".R";
+    
+    var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
+    var fileImportR = [
+		{filePath: 'src/cordova/plugin/androidShowPDFPlugin/Main.java', importStatement: 'simar.android.showpdf.R'}
+    ];
+
+
+    console.log('*****************************************');
+    console.log('*       inject file R  ANDROID             *');
+    console.log('*****************************************');
+    console.log('*       Inject: ' + replaceWith + '    *');
+    
+    fileImportR.forEach(function(val) {
+    	var fullfilename = path.join(platformRoot, val.filePath);
+    	console.log('*  Inject in file: ' + fullfilename + ' the import statemet: ' + val.importStatement + '  *');
+    	if (fs.existsSync(fullfilename)) {
+    		replace_string_in_file(fullfilename, val.importStatement, replaceWith);
+    	} else {
+            console.error('* missing file:', fullfilename);
+        }
+    });
+}
