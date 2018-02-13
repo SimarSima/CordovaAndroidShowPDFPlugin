dc3373bcbda2bf37bae6a701e4fa369d8bf16cc7
Simar <441315491@qq.com>
2018-02-13 15:05:46 +0800

    update

--- src/android/showPDF/hook/configure-android.js
+++ src/android/showPDF/hook/configure-android.js
@@ -16,28 +16,28 @@ module.exports = function (ctx) {
         var result = data.replace(new RegExp(to_replace, "g"), replace_with);
         fs.writeFileSync(filename, result, 'utf8');
     }
-    
-    function getConfidId(configString){
-    	
-    	var firstCut = configString.split(" id=");
-		//console.log(firstCut);
-		var secondCut = firstCut[1].replace(/"/g,"");
-		//console.log(secondCut);
-		var id = secondCut.slice(0,secondCut.indexOf(" "));
-		//console.log(id);
-		return id;
+
+    function getConfidId(configString) {
+
+        var firstCut = configString.split(" id=");
+        //console.log(firstCut);
+        var secondCut = firstCut[1].replace(/"/g, "");
+        //console.log(secondCut);
+        var id = secondCut.slice(0, secondCut.indexOf(" "));
+        //console.log(id);
+        return id;
     }
-    
+
     var ourconfigfile = path.join(ctx.opts.projectRoot, "config.xml");
     var configXMLPath = "config.xml";
     var data = fs.readFileSync(ourconfigfile, 'utf8');
-    
+
     var replaceWith = getConfidId(data) + ".R";
-    
+
     var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
     var fileImportR = [
-		{filePath: 'app/src/java/cordova/plugin/androidShowPDFPlugin/Main.java', importStatement: 'simar.android.showpdf.R'},
-		{filePath: 'app/src/java/cordova/plugin/androidShowPDFPlugin/androidShowPDFPlugin.java', importStatement: 'simar.android.showpdf.R'}
+        { filePath: 'app/src/java/cordova/plugin/androidShowPDFPlugin/Main.java', importStatement: 'simar.android.showpdf.R' },
+        { filePath: 'app/src/java/cordova/plugin/androidShowPDFPlugin/androidShowPDFPlugin.java', importStatement: 'simar.android.showpdf.R' }
     ];
 
 
@@ -45,14 +45,14 @@ module.exports = function (ctx) {
     console.log('*       inject file R  ANDROID             *');
     console.log('*****************************************');
     console.log('*       Inject: ' + replaceWith + '    *');
-    
-    fileImportR.forEach(function(val) {
-    	var fullfilename = path.join(platformRoot, val.filePath);
-    	console.log('*  Inject in file: ' + fullfilename + ' the import statemet: ' + val.importStatement + '  *');
-    	if (fs.existsSync(fullfilename)) {
-		console.log('~~~Replace~~~~');
-    		replace_string_in_file(fullfilename, val.importStatement, replaceWith);
-    	} else {
+
+    fileImportR.forEach(function (val) {
+        var fullfilename = path.join(platformRoot, val.filePath);
+        console.log('*  Inject in file: ' + fullfilename + ' the import statemet: ' + val.importStatement + '  *');
+        if (fs.existsSync(fullfilename)) {
+           // console.log('~~~Replace~~~~');
+            replace_string_in_file(fullfilename, val.importStatement, replaceWith);
+        } else {
             console.error('* missing file:', fullfilename);
         }
     });
